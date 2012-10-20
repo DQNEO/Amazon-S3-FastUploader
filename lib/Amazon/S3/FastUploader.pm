b0f0de5562c00882c7b9832f1cd80d269cdea257
DQNEO <dqneoo@gmail.com>
2012-10-21 05:28:52 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 706670c..466e03c 100644
    --- a/Changes
    +++ b/Changes
    @@ -12,7 +12,10 @@ Revision history for Amazon-S3-FastUploader
     0.04    2012-09-30
             improve error handling
    
    -0.05    2012-10-21
    +0.05    empyt update (just test)
    +
    +
    +0.06	2012-10-21
             can set acl_short
             set mime types automatically

--- lib/Amazon/S3/FastUploader.pm
+++ lib/Amazon/S3/FastUploader.pm
@@ -8,7 +8,7 @@ use Parallel::ForkManager;
 use base qw( Class::Accessor );
 __PACKAGE__->mk_accessors( qw(config) );
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub new {
     my $class = shift;
