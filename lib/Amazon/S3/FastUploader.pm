9deb485b8f2036d151015e06795240f3b14f30ad
DQNEO <dqneoo@gmail.com>
2012-12-01 11:33:16 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 466e03c..8f589d2 100644
    --- a/Changes
    +++ b/Changes
    @@ -19,3 +19,5 @@ Revision history for Amazon-S3-FastUploader
             can set acl_short
             set mime types automatically
    
    +0.07    2012-12-01
    +        can use ssl option for the cmd tool

--- lib/Amazon/S3/FastUploader.pm
+++ lib/Amazon/S3/FastUploader.pm
@@ -8,7 +8,7 @@ use Parallel::ForkManager;
 use base qw( Class::Accessor );
 __PACKAGE__->mk_accessors( qw(config) );
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 sub new {
     my $class = shift;
